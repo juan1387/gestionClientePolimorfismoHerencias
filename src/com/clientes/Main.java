801dadc19f17e7e498917c696ca2ebac8b2da8dc
Juan Camilo Noriega <juan1387@gmail.com>
2020-09-28 20:29:32 -0500

    Menu aplicacion

--- src/com/clientes/Main.java
+++ src/com/clientes/Main.java
@@ -6,21 +6,17 @@ public class Main {
 
     public static void main(String[] args) {
         String menu =
-                "         * 1 para agregar productos a clientes\n" +
-                "         * 2 para consultar productos con documento de identidas\n" +
-                "         * 3 Eliminar un producto\n" +
-                "         * 4 salir de la aplicacion";
+                        "         * 1 Agregar cliente\n" +
+                        "         * 2 Editar cliente\n" +
+                        "         * 3 Eliminar cliente\n" +
+                        "         * 4 Agregar productos\n" +
+                        "         * 5 Consultar clientes con documento y tipo de documento \n" +
+                        "         * 0 salir de la aplicacion";
         Scanner in = new Scanner(System.in);
-        /**
-         * MENU APLICACION GESTION DE CLIENTES
-         * 1 para agregar productos a clientes
-         * 2 para consultar productos con documento de identidas
-         * 3 Eliminar un producto
-         * 4 salir de la aplicacion
-         */
+
 
         //Uso clico do while() para iniciar menu
-        @Deprecated(since = "1.2",forRemoval = true)
+        @Deprecated(since = "1.2", forRemoval = true)
         Integer opMenu = new Integer(0);
         /**
          * Segun el concepto de polimorfismo un objeto puede tener multiples formas
@@ -28,11 +24,11 @@ public class Main {
          */
 
 
-        do{
+        do {
 
             System.out.println(menu);
             opMenu = in.nextInt();
-            switch (opMenu.toString()){
+            switch (opMenu.toString()) {
                 case "1":
                     break;
                 case "2":
@@ -40,13 +36,17 @@ public class Main {
                 case "3":
                     break;
                 case "4":
+                    break;
+                case "5":
+                    break;
+                case "0":
                     System.out.println("Muchas gracias por usar nuestra app, hasta luego");
                     break;
                 default:
                     System.out.println("El valor ingresado no es una opcion de menu");
                     break;
             }
-        }while (!opMenu.toString().equals("4"));
+        } while (!opMenu.toString().equals("0"));
         in.close();
 
     }
